4c75cd09c2866799c4ccfff84790c890387be248
Pousali <pousalidolai59@gmail.com>
2026-03-17 14:22:05 +0000

    commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,49 +1,52 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 from .models import Post
 from .forms import EmailPostForm
-from django.core.mail import send_mail
+
 
 def post_share(request, post_id):
+    """
+    Share a post via email
+    """
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-
             post_url = request.build_absolute_uri(post.get_absolute_url())
 
             subject = f"{cd['name']} recommends you read {post.title}"
             message = f"Read {post.title} at {post_url}\n\nComments: {cd['comments']}"
 
-            send_mail(
-                subject,
-                message,
-                None,
-                [cd['to']]
-            )
-
+            send_mail(subject, message, None, [cd["to"]])
             sent = True
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {
-        'post': post,
-        'form': form,
-        'sent': sent
-    })
+    return render(
+        request,
+        "blog/post/share.html",
+        {"post": post, "form": form, "sent": sent},
+    )
+
 
 class PostListView(ListView):
-    model = Post
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+
     def get_queryset(self):
         return Post.published.all()
+
 
 def post_list(request):
     """
@@ -51,7 +54,7 @@ def post_list(request):
     """
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
 
     try:
         posts = paginator.page(page_number)
@@ -60,11 +63,7 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -77,27 +76,6 @@ def post_detail(request, year, month, day, post):
         slug=post,
         publish__year=year,
         publish__month=month,
-        publish__day=day
+        publish__day=day,
     )
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post}
-    )
-
-
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        status=Post.Status.PUBLISHED,
-        slug=post,
-        publish__year=year,
-        publish__month=month,
-        publish__day=day
-    )
-    return render(
-        request,
-        'blog/post/detail.html',
-        {'post': post}
-    )
+    return render(request, "blog/post/detail.html", {"post": post})
