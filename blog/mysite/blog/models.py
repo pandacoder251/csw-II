2292950dd8266aff7d73f2e47c09f811fa284231
Pousali <pousalidolai59@gmail.com>
2026-03-18 12:14:01 +0000

    commits

--- blog/mysite/blog/models.py
+++ blog/mysite/blog/models.py
@@ -3,9 +3,11 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -19,8 +21,8 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='blog_posts'
     )
-    objects = models.Manager()       
-    published = PublishedManager()   
+    objects = models.Manager()       # Default manager
+    published = PublishedManager()   # Custom manager
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -37,8 +39,32 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
     def get_absolute_url(self):
         return reverse(
-        'blog:post_detail',
-        args=[self.publish.year, self.publish.month, self.publish.day, self.slug]
+            'blog:post_detail',
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug]
+        )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
     )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
