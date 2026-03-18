64a1a89d0bdd6c8b072be9f4896d575dac5f0270
Pousali <pousalidolai59@gmail.com>
2026-03-18 04:19:31 +0000

    new commits

---
+++ blog/mysite/blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+                                settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='blog_posts'
+     )
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,   # <-- important fix
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
