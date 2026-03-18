f1542fa5692c4a1ec58bb38674120d51f417940f
Pousali <pousalidolai59@gmail.com>
2026-03-18 06:00:15 +0000

    commited till pagination

--- blog/mysite/blog/models.py
+++ blog/mysite/blog/models.py
@@ -30,9 +30,39 @@ class Post(models.Model):
         default=Status.DRAFT
     )
 
+    objects = models.Manager()  # default manager
+
     class Meta:
         ordering = ['-publish']
         indexes = [models.Index(fields=['-publish'])]
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug]
+        )
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+Post.published = PublishedManager()  # attach PublishedManager after Post is defined
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
