e916fd0f69f0006dcd56c6a0742eef3bacd86f4f
Pousali <pousalidolai59@gmail.com>
2026-03-18 07:32:18 +0000

    till emails

--- blog/mysite/blog/forms.py
+++ blog/mysite/blog/forms.py
@@ -1,9 +1,8 @@
+# blog/forms.py
 from django import forms
+
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    comments = forms.CharField(
-    required=False,
-    widget=forms.Textarea
- )
+    comments = forms.CharField(required=False, widget=forms.Textarea)
--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from .forms import EmailPostForm
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
+    )
 
 def post_list(request):
     post_list = Post.published.all()  # only published posts
