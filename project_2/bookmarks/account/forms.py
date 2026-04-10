45eb5dac3bbb361551d32425cb526983d576f0bf
Pousali <pousalidolai59@gmail.com>
2026-04-10 04:22:58 +0000

    commit

--- project_2/bookmarks/account/forms.py
+++ project_2/bookmarks/account/forms.py
@@ -1,4 +1,4 @@
-from djab=ngo import forms
+from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput())
--- project_2/bookmarks/account/views.py
+++ project_2/bookmarks/account/views.py
@@ -2,20 +2,29 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-def user_Login(request):
+@login_required
+def dashboard(request):
+    return render(
+    request,'account/dashboard.html',
+    {'section': 'dashboard'})
+
+def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=['password'])
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return HttpResponse("Login successful!")
             else:
                 return HttpResponse("Invalid credentials.")
+        else:
+            form = form
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
