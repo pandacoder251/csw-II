48bc379d0b7ea2ecb2b6f4daa82d393ba67bfc47
Pousali <pousalidolai59@gmail.com>
2026-04-10 02:58:30 +0000

    commit to project

---
+++ project_2/bookmarks/account/forms.py
@@ -0,0 +1,4 @@
+from djab=ngo import forms
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput())
--- project_2/bookmarks/account/views.py
+++ project_2/bookmarks/account/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.http import HttpResponse
 
-# Create your views here.
+def user_Login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse("Login successful!")
+            else:
+                return HttpResponse("Invalid credentials.")
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
