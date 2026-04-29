6d5b3674d756d8f62a76216cc98b5bce94881c4c
Pousali <pousalidolai59@gmail.com>
2026-04-29 11:19:40 +0000

    commits

--- project_2/bookmarks/account/urls.py
+++ project_2/bookmarks/account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
  path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
  path('',views.dashboard, name='dashboard'),
+ path('password_change', auth_views.PasswordChangeView.as_view(template_name='account/password_change_form.html'), name='password_change'),
+ path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'), name='password_change_done'),
 ]
