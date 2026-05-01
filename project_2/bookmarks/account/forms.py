433b8fc2fae37c326a7d7ea8fc7f9708ee154306
Pousali <pousalidolai59@gmail.com>
2026-05-01 02:49:30 +0000

    commit

--- project_2/bookmarks/account/forms.py
+++ project_2/bookmarks/account/forms.py
@@ -18,7 +18,11 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
-
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 class UserEditForm(forms.ModelForm):
     class Meta:
