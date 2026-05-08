bc4a871303c01556d78269eb27a129feb1751efe
Pousali <pousalidolai59@gmail.com>
2026-05-08 02:28:39 +0000

    commited

--- project_2/bookmarks/account/urls.py
+++ project_2/bookmarks/account/urls.py
@@ -3,13 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # previous login url
-    # path('login/', views.user_login, name='login'),
-    # login / logout urls
+    # Built-in login/logout views
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # change password urls
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('', views.dashboard, name='dashboard'),
+
+    # Dashboard view
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- project_2/bookmarks/account/views.py
+++ project_2/bookmarks/account/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section': 'dashboard'}
+    )
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -25,15 +34,3 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
-
-@login_required
-def dashboard(request):
-    return render(
-        request,
-        'account/dashboard.html',
-        {'section': 'dashboard'}
-    )
-
-@login_required
-def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
