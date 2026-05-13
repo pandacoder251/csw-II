ca5d02d7126e0967f7a94cdf550ecb2e905bd516
Pousali <pousalidolai59@gmail.com>
2026-05-13 16:25:18 +0000

    commit

--- Socialmedia/bookmarks/account/views.py
+++ Socialmedia/bookmarks/account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -85,14 +86,16 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(
+                request, 'Profile updated successfully'
+            )
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
     return render(
         request,
         'account/edit.html',
-        {
-            'user_form': user_form,
-            'profile_form': profile_form
-        },
+        {'user_form': user_form, 'profile_form': profile_form},
     )
