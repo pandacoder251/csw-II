17a2cf7d083d2e89277e20b944218212aaff6e2a
Pousali <pousalidolai59@gmail.com>
2026-05-13 11:23:51 +0000

    commit

--- project_2/bookmarks/images/views.py
+++ project_2/bookmarks/images/views.py
@@ -1,60 +1,92 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render, redirect
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
 from .models import Image
 
 
 @login_required
-def image_detail(request, slug):
+def image_create(request):
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # assign current user to the item
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Image added successfully')
+            # redirect to new created image detail view
+            return redirect(new_image.get_absolute_url())
+    else:
+        # build form with data provided by the bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+    return render(
+        request,
+        'images/image/create.html',
+        {'section': 'images', 'form': form},
+    )
 
-    image = get_object_or_404(Image, slug=slug)
 
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
     return render(
         request,
         'images/image/detail.html',
-        {
-            'section': 'images',
-            'image': image
-        }
+        {'section': 'images', 'image': image},
     )
 
 
 @login_required
-def image_create(request):
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
 
-    if request.method == 'POST':
 
-        form = ImageCreateForm(
-            data=request.POST,
-            files=request.FILES
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images},
         )
-
-        if form.is_valid():
-
-            new_image = form.save(commit=False)
-
-            new_image.user = request.user
-
-            new_image.save()
-
-            messages.success(
-                request,
-                'Image added successfully'
-            )
-
-            return redirect(new_image.get_absolute_url())
-
-    else:
-
-        form = ImageCreateForm(data=request.GET)
-
     return render(
         request,
-        'images/image/create.html',
-        {
-            'section': 'images',
-            'form': form
-        }
+        'images/image/list.html',
+        {'section': 'images', 'images': images},
     )
