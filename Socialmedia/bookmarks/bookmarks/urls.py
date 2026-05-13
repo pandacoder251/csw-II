2e902e119eab332a1c321ce172177093d9c81390
Pousali <pousalidolai59@gmail.com>
2026-05-13 15:43:01 +0000

    commit

--- Socialmedia/bookmarks/bookmarks/urls.py
+++ Socialmedia/bookmarks/bookmarks/urls.py
@@ -14,10 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path(
+        'social-auth/',
+        include('social_django.urls', namespace='social'),
+    ),
+    path('images/', include('images.urls', namespace='images')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
